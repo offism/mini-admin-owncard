7ee89a8e083d31091500f98c5c3ef72f75b95c05
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-11-24 18:29:28 +0300

    4c

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './style/styles.css'
 function App() {
   let [count,setCount] = useState(0)
   let [toggle,setToggle] = useState(false)
+  let [value,setValue] = useState("")
   
   function inc(){
     count +=1
@@ -20,11 +21,14 @@ function App() {
      <button onClick={inc} className="btn btn-success">INC</button>
      <button onClick={dec} className="btn btn-danger">DEC</button>
      <button onClick={()=> setToggle(!toggle)} className="btn btn-secondary">Toggle</button>
+     <hr/>
      {toggle 
      ?
      <p className="lead">You Tube Content</p>  
      : null
      }
+     <h4>Value: {value}</h4>
+     <input className="form-control" type="text" value={value} onChange={event => setValue(event.target.value)}/> 
     </div>    
    </>
     );
