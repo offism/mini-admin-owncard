4d3604afe0382baf252320b8af941a479a2f37d2
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-11-24 18:00:52 +0300

    2c

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-
+import { useState } from "react";
 function App() {
+  let [count,setCount] = useState(0)
+  function inc(){
+    count +=1
+    setCount(count)
+  }
+  function dec(){
+    count -=1
+    setCount(count)
+  }
   return (
-    <h1>Hello SMA</h1>       
-  );
+   <>
+    <h3>Count: {count}</h3>
+   <button onClick={inc} className="btn btn-success">INC</button>
+   <button onClick={dec} className="btn btn-danger">DEC</button>
+   </>
+    );
 }
 
 export default App;
