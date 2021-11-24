bb2ebe5b27f1f454f769d698db71fcd22507e939
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-11-24 18:20:05 +0300

    3c

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useState } from "react";
+import './style/styles.css'
+
 function App() {
   let [count,setCount] = useState(0)
+  let [toggle,setToggle] = useState(false)
+  
   function inc(){
     count +=1
     setCount(count)
@@ -11,9 +15,17 @@ function App() {
   }
   return (
    <>
-    <h3>Count: {count}</h3>
-   <button onClick={inc} className="btn btn-success">INC</button>
-   <button onClick={dec} className="btn btn-danger">DEC</button>
+    <div className="app w-50 mx-auto">
+     <h3>Count: {count}</h3>
+     <button onClick={inc} className="btn btn-success">INC</button>
+     <button onClick={dec} className="btn btn-danger">DEC</button>
+     <button onClick={()=> setToggle(!toggle)} className="btn btn-secondary">Toggle</button>
+     {toggle 
+     ?
+     <p className="lead">You Tube Content</p>  
+     : null
+     }
+    </div>    
    </>
     );
 }
